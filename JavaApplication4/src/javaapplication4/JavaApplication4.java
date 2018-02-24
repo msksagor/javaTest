cc58f20f55efd1e24c92b1281386b5d98036208d
msksagor <msksagor@gmail.com>
2018-02-24 12:42:35 +0600

    loop added

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication4;
 
-/**
- *
- * @author fondn
- */
 public class JavaApplication4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Welcome to Java World");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Your are in - "+i);
+        }
+            
     }
     
 }
